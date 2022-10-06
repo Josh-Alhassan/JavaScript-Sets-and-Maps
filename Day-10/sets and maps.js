258de4110b8adeae29c6138cec44dd69ce1f37a2
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-10-06 16:27:07 +0000

    adding element to a set

--- Day-10/sets and maps.js
+++ Day-10/sets and maps.js
@@ -1,6 +1,6 @@
 // ## Creating an empty set
 console.log('Creating an empty set')
-const companies = new Set()
+const companies = new Set() // creating an empty set
 console.log(companies)
 
 // Creating set from array
@@ -23,3 +23,26 @@ console.log(setOfLanguages)
 for (const language of setOfLanguages) {
   console.log(language)
 }
+
+// Adding an Element to a set
+console.log('')
+console.log('Adding an Element to a set')
+
+console.log(companies)
+console.log('companies size:', companies.size)
+
+companies.add('Google') // add element to the set
+companies.add('Facebook')
+companies.add('Amazon')
+companies.add('Oracle')
+companies.add('Microsoft')
+
+console.log('Companyies size now:', companies.size)
+console.log(companies)
+
+// We can use loop to add element to a set
+setOfCompanies = new Set()
+for (const company of companies) {
+  setOfCompanies.add(company)
+}
+console.log('Set of Companies set added:', setOfCompanies)
