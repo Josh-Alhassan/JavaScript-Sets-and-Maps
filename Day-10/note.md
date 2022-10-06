b46320950ef7032d15e0207e5ff4a7d97a5662b2
Saint-Josh <41088053+Saint-Josh@users.noreply.github.com>
2022-10-06 15:00:30 +0100

    creating et form array

--- Day-10/note.md
+++ Day-10/note.md
@@ -1,4 +1,5 @@
 # Set
 set is a collection of elements. Set can only contain unique elements.
 
++ Set is an iterable object and we can iterate through each elements.
 
--- Day-10/sets and maps.js
+++ Day-10/sets and maps.js
@@ -6,3 +6,20 @@ console.log(companies)
 // Creating set from array
 console.log('')
 console.log('Creating set from Arrays')
+
+const languages = [
+  'English',
+  'Finnish',
+  'English',
+  'French',
+  'Spanish',
+  'English',
+  'French',
+]
+
+const setOfLanguages = new Set(languages)
+console.log(setOfLanguages)
+
+for (const language of setOfLanguages) {
+  console.log(language)
+}
