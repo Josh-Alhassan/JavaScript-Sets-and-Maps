8a0fa9083e990aebd1442067dc255030e833dfe0
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-10-06 16:45:24 +0000

    clearing the set

--- Day-10/note.md
+++ Day-10/note.md
@@ -2,4 +2,5 @@
 set is a collection of elements. Set can only contain unique elements.
 
 + Set is an iterable object and we can iterate through each elements.
-+ <code>has()</code> method help us to know if a certain element exists in a set. It returns a *boolean* value
++ <code>has()</code> method help us to know if a certain element exists in a set. It returns a *boolean* value.
++ <code>clear()</code> method removes all the element in a set
--- Day-10/sets and maps.js
+++ Day-10/sets and maps.js
@@ -58,3 +58,32 @@ console.log('')
 console.log('Checking If element exists in a Set()')
 console.log(companies.has('Apple'))
 console.log(companies.has('Facebook'))
+
+// Clearing the set
+console.log('')
+console.log('Clearing the set')
+
+companies.clear()
+console.log(companies, companies.size)
+
+const langSet = new Set(languages);
+console.log(langSet)
+console.log(langSet.size)
+
+const counts = [];
+const count = {};
+
+for (const lang of langSet) {
+  const filteredLang = languages.filter(function(lng) {
+    return lng === lang
+  })
+  console.log(filteredLang);
+  counts.push({lang: lang, count: filteredLang.length})
+}
+console.log(counts)
+
+// counting unique item in an array
+const numbers = [5, 3, 2, 5, 5, 9, 4, 5];
+const setOfNumbers = new Set(numbers)
+
+console.log(setOfNumbers)
