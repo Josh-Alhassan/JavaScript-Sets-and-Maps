8c0740ef52ae20b2f4f9c930386f0e3969f455e8
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-10-12 08:04:05 +0000

    spread operator to copy array

--- Day-11/destructureAndSpread.js
+++ Day-11/destructureAndSpread.js
@@ -224,3 +224,25 @@ let [gem, fra, , ...nordicCountries] = spreadCountries
 console.log(gem)
 console.log(fra)
 console.log(nordicCountries)
+
+// Spread operator to copy array
+spaceInConsole()
+title('### Spread Operator to copy array')
+
+const evens = [0, 2, 4, 6, 8, 10];
+const evenNumbers = [...evens]
+
+const odds = [1, 3, 5, 7, 9];
+const oddNumbers = [...odds];
+
+const wholeNumbers = [...evens, ...odds]
+
+console.log(evenNumbers)
+console.log(oddNumbers)
+console.log(wholeNumbers);
+
+const frontend = ['HTML', 'CSS', 'JavaScript', 'React', 'Vue'];
+const backend = ['Node', 'Express', 'MongoDB'];
+const fullstack = [...frontend, ...backend];
+
+console.log(fullstack)
