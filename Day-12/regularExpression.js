b7da452afb661e912dfe2da852a96d6ecb844406
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-10-19 01:24:50 +0000

    Square bracket RegExp character

--- Day-12/regularExpression.js
+++ Day-12/regularExpression.js
@@ -44,3 +44,16 @@ D%o%es thi%s m%ot%iv%a%te %y%o%u to b%e a t%e%a%cher.'
 
 matchReplaced = moreTxt.replace(/%/g, '')
 console.log(matchReplaced)
+
+// Square Brackets
+let squarePattern = /[Aa]pple/g // This means either A or a
+moreTxt = 'Apple and banana are fruits. An old cliche says an apple a day keeps the  doctor way has been replaced by a banana a day keeps the doctor far far away. ';
+let matches = moreTxt.match(squarePattern)
+
+console.log(matches)
+
+// If we want to look for banana aswell
+squarePattern = /[Aa]pple|[Bb]anana/g
+matches = moreTxt.match(squarePattern)
+
+console.log(matches)
