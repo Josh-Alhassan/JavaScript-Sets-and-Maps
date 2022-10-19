d4a4ad0c29cd229f9235cffef5e9bdbbe4406150
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-10-19 01:18:09 +0000

    RegEx object methods

--- Day-12/README.md
+++ Day-12/README.md
@@ -17,4 +17,26 @@ Flags are optional parameter in a regular expression which determine the type of
 + i: case insenitive flag (It searches for both lowercase and uppercase).
 + m: multiline.
 
-### Creating a pattern with RegEx constructor
+## RegExp Object Methods
+- Testing for a *match*: <code>test()</code>
+- Array containing all of the match: <code>match()</code>. This returns an array containing all of the matches, including capturing  groups, or null if no match is found.
+**NOTE**: If we do not use global flag, match() returns an array containing the pattern, index, input and group.
+- <code>search()</code>: Tests for a match in a string. It returns the index of the match, or -1 if the search fails.
+- replacing a substring <code>replace()</code>: Executes a search for a match in a string, and replaces the matched substring with a replacement substring.
+
++ []: A set of characters
++ \: uses to escape special charaters
++ .: Any character except new line character (\n)
++ ^: Starts with
++ $: ends with
++ *: zero or more times
++ +: One or more times
++ ?: zero or one time
++ \b: word bounder. Matches with the beginning or ending of a word
++ {3}: Exactly 3 characters
++ {3,8}: 3 to 8 characters
++ {3,}: At least 3 characters
++ |: either or
++ (): capture or group
+
+### Square Bracket
--- Day-12/regularExpression.js
+++ Day-12/regularExpression.js
@@ -7,3 +7,40 @@ console.log(regEx)
 // Writing the pattern and flag inside the RegExp constructor
 let regExFlag = new RegExp('love', 'gi')
 console.log(regExFlag) // Result, same as above
+
+// Creating a pattern without RegExp constructor
+let withoutRegEx = /love/gi
+console.log(withoutRegEx)
+
+// RegExp Object Methods
+// Testing for a match
+const str = 'I love JavaScript';
+const testPattern = /love/g;
+const result = testPattern.test(str)
+console.log(result)
+
+// Array containing all of the match
+console.log(str);
+console.log(testPattern);
+const matchResult = str.match(testPattern)
+console.log(matchResult)
+
+// Search method
+const searchResult = str.search(pattern)
+console.log(searchResult);
+
+// Replacing a substring
+const txt = 'Python is the most beautiful language that a humanbeing has ever created.\
+I recommend python for a first programming language';
+
+let matchReplaced = txt.replace(/Python|python/gi, 'JavaScript');
+console.log(matchReplaced)
+
+let moreTxt = '%I a%m te%%a%%che%r% a%n%d %% I l%o%ve te%ach%ing.\
+T%he%re i%s n%o%th%ing as m%ore r%ewarding a%s e%duc%at%i%ng a%n%d e%m%p%ow%er%ing \
+p%e%o%ple.\
+I fo%und te%a%ching m%ore i%n%t%er%%es%ting t%h%an any other %jobs.\
+D%o%es thi%s m%ot%iv%a%te %y%o%u to b%e a t%e%a%cher.'
+
+matchReplaced = moreTxt.replace(/%/g, '')
+console.log(matchReplaced)
