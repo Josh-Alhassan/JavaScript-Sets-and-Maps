0c21d797fd1b782044f5871c2bc1b46552c7721a
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-10-19 00:15:40 +0000

    Creating pattern w/ RegExp constructor

--- Day-12/regularExpression.js
+++ Day-12/regularExpression.js
@@ -1,1 +1,9 @@
-console.log('Hi')
+// ## Creating a pattern with RegExp constructor
+let pattern = 'love';
+let flag = 'gi'
+let regEx = new RegExp(pattern, flag);
+console.log(regEx)
+
+// Writing the pattern and flag inside the RegExp constructor
+let regExFlag = new RegExp('love', 'gi')
+console.log(regExFlag) // Result, same as above
